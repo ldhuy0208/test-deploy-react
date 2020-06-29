c9346528d0da023fae2e3155b749ca81bd28a572
unknown <ldhuy0208@gmail.com>
2020-06-30 03:16:06 +0700

    Updates

---
+++ precache-manifest.46858d468d9ed70b83c9a5279052cc3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26e35c517db4f87cc14f8d734f28195d",
+    "url": "/test-deploy-react/index.html"
+  },
+  {
+    "revision": "576e093a9fbc9de74f41",
+    "url": "/test-deploy-react/static/css/main.ae0c90ca.chunk.css"
+  },
+  {
+    "revision": "5b61b112853ea52e9ba7",
+    "url": "/test-deploy-react/static/js/2.618b6ab9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-deploy-react/static/js/2.618b6ab9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "576e093a9fbc9de74f41",
+    "url": "/test-deploy-react/static/js/main.3ce96bbf.chunk.js"
+  },
+  {
+    "revision": "d768a5535d3a35f5dc78",
+    "url": "/test-deploy-react/static/js/runtime-main.ae5a9b03.js"
+  }
+]);
