defcb301c9e02ac78b530aa4c4437d30c302db61
unknown <ldhuy0208@gmail.com>
2020-06-30 17:27:40 +0700

    ver 2

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
-import React, { Component } from "react";
+import React, { Component, PureComponent } from "react";
 import Cell from "./component/Cell";
 import "./App.scss";
-const HANG = 10;
-const COT = 20;
-const SO_LUONG_BOOM = 20;
 
-export class App extends Component {
+const HANG = 4;
+const COT = 3;
+const SO_LUONG_BOOM = 2;
+
+export class App extends  PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
       list: [],
+      time: 0,
+      isStarted: false,
+      isWin: false,
     };
   }
 
   khoiTaoBoom = () => {
     const listBoom = [];
     while (listBoom.length < SO_LUONG_BOOM) {
-      const hang = Math.round(Math.random() * HANG);
-      const cot = Math.round(Math.random() * COT);
+      const hang = Math.round(Math.random() * (HANG - 1));
+      const cot = Math.round(Math.random() * (COT - 1));
 
       const isExists =
         listBoom.filter((ele) => ele.hang === hang && ele.cot === cot).length >
         0;
       if (!isExists) listBoom.push({ hang, cot });
     }
-
+    console.log(listBoom);
     return listBoom;
   };
 
@@ -64,122 +68,29 @@ export class App extends Component {
     return count;
   };
 
-  openACell = (
-    updateList,
-    viTriBanDau,
-    viTriTiepTheo = viTriBanDau,
-    daKiemTra = []
-  ) => {
-    console.log("chẹck open-------------------------------------------------");
-    console.log(daKiemTra);
-    console.log(viTriTiepTheo);
+  openACell = (updateList, viTri) => {
+    const { hang, cot } = viTri;
+    if (updateList[hang][cot].isOpen) return false;
 
-    const { hang, cot } = viTriTiepTheo;
+    if (updateList[hang][cot].isMine) return true;
 
-    if (
-      daKiemTra.filter((ele) => ele.hang === hang && ele.cot === cot).length > 0
-    ) {
-      return;
-    }
+    updateList[hang][cot].isOpen = true;
+    const soLuongBoom = this.demSoLuongBoomTamOXungQuanh(viTri);
 
-    daKiemTra.push(viTriTiepTheo);
-
-    if (!(hang >= 0 && hang < HANG && cot >= 0 && cot < COT)) return;
-
-    const soLuongBoom = this.demSoLuongBoomTamOXungQuanh(viTriTiepTheo);
-
-    if (
-      hang === viTriBanDau.hang &&
-      cot === viTriBanDau.cot &&
-      soLuongBoom !== 0
-    ) {
-      updateList[hang][cot].isOpen = true;
+    if (soLuongBoom > 0) {
       updateList[hang][cot].totalBoom = soLuongBoom;
+    } else {
+      let r1 = hang === 0 ? 0 : -1;
+      let c1 = cot === 0 ? 0 : -1;
+      let r2 = hang === HANG - 1 ? 1 : 2;
+      let c2 = cot === COT - 1 ? 1 : 2;
+
+      for (; r1 < r2; r1++)
+        for (let j = c1; j < c2; j++) {
+          this.openACell(updateList, { hang: hang + r1, cot: cot + j });
+        }
     }
-    if (
-      hang !== viTriBanDau.hang &&
-      cot !== viTriBanDau.cot &&
-      soLuongBoom !== 0
-    ) {
-      updateList[hang][cot].isOpen = true;
-      updateList[hang][cot].totalBoom = soLuongBoom;
-    }
-    if (soLuongBoom === 0) {
-      updateList[hang][cot].isOpen = true;
-      updateList[hang][cot].totalBoom = soLuongBoom;
-      this.openACell(
-        updateList,
-        viTriBanDau,
-        {
-          hang: hang,
-          cot: cot - 1,
-        },
-        daKiemTra
-      );
-      this.openACell(
-        updateList,
-        viTriBanDau,
-        {
-          hang: hang + 1,
-          cot: cot,
-        },
-        daKiemTra
-      );
-      this.openACell(
-        updateList,
-        viTriBanDau,
-        {
-          hang: hang,
-          cot: cot + 1,
-        },
-        daKiemTra
-      );
-      this.openACell(
-        updateList,
-        viTriBanDau,
-        {
-          hang: hang - 1,
-          cot: cot,
-        },
-        daKiemTra
-			);
-			this.openACell(
-        updateList,
-        viTriBanDau,
-        {
-          hang: hang - 1,
-          cot: cot - 1,
-        },
-        daKiemTra
-			);
-			this.openACell(
-        updateList,
-        viTriBanDau,
-        {
-          hang: hang -1,
-          cot: cot + 1,
-        },
-        daKiemTra
-			);
-			this.openACell(
-        updateList,
-        viTriBanDau,
-        {
-          hang: hang + 1,
-          cot: cot -1,
-        },
-        daKiemTra
-			);
-			this.openACell(
-        updateList,
-        viTriBanDau,
-        {
-          hang: hang + 1,
-          cot: cot + 1,
-        },
-        daKiemTra
-      );
-    }
+    return false;
   };
 
   moMotO = (viTri, e) => {
@@ -197,9 +108,17 @@ export class App extends Component {
       const updateList = this.state.list;
       this.openACell(updateList, viTri);
 
-      this.setState({
-        list: updateList,
-      });
+      this.setState(
+        {
+          list: updateList,
+          isStarted: true,
+        },
+        () => {
+          if (this.isWinGame()) {
+            this.khoiTaoDanhSach();
+          }
+        }
+      );
     } else if (e.type === "contextmenu") {
       console.log("Right click");
 
@@ -213,6 +132,17 @@ export class App extends Component {
     }
   };
 
+  isWinGame = () => {
+    for (let i = 0; i < HANG; i++)
+      for (let j = 0; j < COT; j++) {
+        if (
+          this.state.list[i][j].isOpen === false &&
+          this.state.list[i][j].isBoom === false
+        )
+          return this.setState({ isWin: false });
+      }
+    this.setState({ isWin: true });
+  };
   onFlag = (viTri) => {
     const { hang, cot } = viTri;
     const updateList = [...this.state.list];
@@ -222,24 +152,54 @@ export class App extends Component {
       list: updateList,
     });
   };
+  startNewGame = () => {
+    this.khoiTaoDanhSach();
+    this.setState({
+      isWin: false,
+      time: 0,
+    });
+  };
   componentDidMount() {
     this.khoiTaoDanhSach();
   }
   render() {
     return (
       <div className="app">
-        {this.state.list.map((hang, i) => (
-          <div className="row">
-            {hang.map((cot, j) => (
-              <Cell
-                onOpen={(e) => this.moMotO({ hang: i, cot: j }, e)}
-                isBoom={this.state.list[i][j].isBoom}
-                totalBoom={this.state.list[i][j].totalBoom}
-                isFlag={this.state.list[i][j].isFlag}
-              />
-            ))}
+        <header>
+          <div>Bom còn lại</div>
+          <div>
+            <button onClick={this.startNewGame}>
+              {this.state.isWin ? (
+                <img
+                  src="https://img.icons8.com/emoji/48/000000/smiling-face-with-sunglasses.png"
+                  alt="new game"
+                />
+              ) : (
+                <img
+                  src="https://img.icons8.com/emoji/48/000000/smirking-face.png"
+                  alt="new game"
+                />
+              )}
+            </button>
           </div>
-        ))}
+          <div>Thời gian: {this.state.time}</div>
+        </header>
+        <main>
+          {this.state.list.map((hang, i) => (
+            <div key={Math.random() + new Date().getTime} className="row">
+              {hang.map((cot, j) => (
+                <Cell
+                  key={Math.random() + new Date().getTime}
+                  onOpen={(e) => this.moMotO({ hang: i, cot: j }, e)}
+                  isBoom={this.state.list[i][j].isBoom}
+                  isOpen={this.state.list[i][j].isOpen}
+                  totalBoom={this.state.list[i][j].totalBoom}
+                  isFlag={this.state.list[i][j].isFlag}
+                />
+              ))}
+            </div>
+          ))}
+        </main>
       </div>
     );
   }
--- src/component/Cell.js
+++ src/component/Cell.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Cell.scss'
 function Cell(props) {
-  const openClass = props.totalBoom !== null && props.totalBoom === 0 && "open"; 
+  const openClass = props.isOpen && "open"; 
   return (
     <div className={"cell " + openClass} onClick={props.onOpen} onContextMenu={props.onOpen}>
-
+      
       {(props.totalBoom !== null && props.totalBoom !== 0) ? props.totalBoom : props.isFlag ? 'F': ''}
 
+      {props.isBoom && 'B'}
     </div>
   )
 }
